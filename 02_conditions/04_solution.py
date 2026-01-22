fbacfb4f500ae8879130eb0157fe5d5a1cbadeec
Bhavik3012 <bhaviksongara301205@gmail.com>
2026-01-23 00:49:00 +0530

    Add condition-based solutions for age, price, grade, fruit ripeness, and weather

---
+++ 02_conditions/04_solution.py
@@ -0,0 +1,10 @@
+fruit = "Banana"
+color = "Yellow"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("The banana is not ripe yet.")
+    elif color == "Yellow":
+        print("The banana is ripe.")
+    else:
+        print("The banana is not ripe yet.")
---
+++ 02_conditions/05_solution.py
@@ -0,0 +1,10 @@
+weather = "Sunny"
+
+if weather == "Sunny":
+    print("It's a bright and beautiful day!")
+elif weather == "Rainy":
+    print("Don't forget to take an umbrella.")
+elif weather == "Cloudy":
+    print("It might be a bit gloomy today.")
+else:
+    print("Weather condition unknown.")
